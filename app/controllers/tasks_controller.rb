8042bf5c62bf1864e1dadb497e373feca92c28bd
tatsuru.uehara <papadoxwill.mile21@gmail.com>
2020-06-10 09:19:01 +0000

    set_task

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,6 +1,6 @@
 class TasksController < ApplicationController
   before_action :require_user_logged_in
-  before_action :current_user, only: [:show, :edit, :update, :destroy]
+  before_action :set_task, only: [:show, :edit, :update, :destroy]
   
   def index
     @tasks = current_user.tasks.order(id: :desc)
@@ -47,7 +47,7 @@ class TasksController < ApplicationController
   
   private
   
-  def current_user
+  def set_task
     @task = current_user.tasks.find(params[:id])
   end 
   
